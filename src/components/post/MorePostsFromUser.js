9ff40061551a45451b1a97be8798b2e52aea822c
DimaMamaev <dmamaev@my.com>
2020-06-13 19:49:17 +0300

    created post page

--- src/components/post/MorePostsFromUser.js
+++ src/components/post/MorePostsFromUser.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMorePostsFromUserStyles } from "../../styles";
+import { Typography, Grid } from "@material-ui/core";
+import { LoadingLargeIcon } from "../../icons";
+import { getDefaultPost, defaultUser } from "../../data";
+import GridPost from "../shared/GridPost";
+import { Link } from "react-router-dom";
 
 function MorePostsFromUser() {
-  useMorePostsFromUserStyles();
+  const classes = useMorePostsFromUserStyles();
+  const [loading] = useState(false);
 
-  return <div>MorePostsFromUser</div>;
+  return (
+    <div className={classes.container}>
+      <Typography
+        variant="subtitle2"
+        color="textSecondary"
+        component="h2"
+        className={classes.typography}
+        gutterBottom
+      >
+        More posts from{" "}
+        <Link to={`/${defaultUser.username}`} className={classes.link}>
+          @{defaultUser.username}
+        </Link>
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <article className={classes.article}>
+          <div className={classes.postContainer}>
+            {Array.from({ length: 6 }, () => getDefaultPost()).map((post) => (
+              <GridPost key={post.id} post={post} />
+            ))}
+          </div>
+        </article>
+      )}
+    </div>
+  );
 }
 
 export default MorePostsFromUser;
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,10 +1,158 @@
-import React from "react";
+import React, { useState } from "react";
 import { usePostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import {
+  MoreIcon,
+  CommentIcon,
+  ShareIcon,
+  UnlikeIcon,
+  LikeIcon,
+  RemoveIcon,
+  SaveIcon,
+} from "../../icons";
+import { Link } from "react-router-dom";
+import {
+  Typography,
+  Button,
+  Hidden,
+  Divider,
+  TextField,
+} from "@material-ui/core";
+import FollowSuggestions from "../shared/FollowSuggestions";
+import OptionsDialog from "../shared/OptionsDialog";
+import { defaultPost } from "../../data";
 
 function Post() {
-  usePostStyles();
+  const classes = usePostStyles();
+  const [showDialogOptions, setDialogOptions] = useState(false);
+  const { media, id, likes, user, caption, comments } = defaultPost;
 
-  return <div>Post</div>;
+  return (
+    <div className={classes.postContainer}>
+      <article className={classes.article}>
+        <div className={classes.postHeader}>
+          <UserCard user={user} avatarSize={32} />
+          <MoreIcon
+            className={classes.moreIcon}
+            onClick={() => setDialogOptions(true)}
+          />
+        </div>
+
+        <div className={classes.postImage}>
+          <img src={media} alt="Post img" className={classes.image} />
+        </div>
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            <LikeBtn />
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            <SaveBtn />
+          </div>
+          <Typography className={classes.likes} variant="subtitile2">
+            <span> {likes === 1 ? "1 like" : `${likes} likes `} </span>
+          </Typography>
+
+          <div className={classes.postCaptionContainer}>
+            <Typography
+              variant="body2"
+              className={classes.postCaption}
+              component="span"
+              dangerouslySetInnerHTML={{ __html: caption }}
+            />
+            {comments.map((comment) => (
+              <div key={comment.id}>
+                <Link to={`/${comment.user.username}`}>
+                  <Typography
+                    variant="subtitle2"
+                    component="span"
+                    className={classes.commentUsername}
+                  >
+                    {comment.user.username}
+                  </Typography>{" "}
+                  <Typography variant="body2" component="span">
+                    {comment.content}
+                  </Typography>
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          <Typography color="textSecondary" className={classes.datePosted}>
+            1 DAY AGO
+          </Typography>
+          <Hidden xsDown>
+            <div className={classes.comment}>
+              <Divider />
+              <Comment />
+            </div>
+          </Hidden>
+        </div>
+      </article>
+      {showDialogOptions && (
+        <OptionsDialog onClose={() => setDialogOptions(false)} />
+      )}
+    </div>
+  );
 }
 
+function LikeBtn() {
+  const classes = usePostStyles();
+  const [like, setLike] = useState(false);
+  const Icon = like ? UnlikeIcon : LikeIcon;
+  const className = like ? classes.liked : classes.like;
+  const onClick = like ? handleUnLike : handleLike;
+  function handleUnLike() {
+    setLike(false);
+  }
+  function handleLike() {
+    setLike(true);
+  }
+  return <Icon className={className} onClick={onClick} />;
+}
+function SaveBtn() {
+  const classes = usePostStyles();
+  const [save, setSave] = useState(false);
+  const Icon = save ? RemoveIcon : SaveIcon;
+  const onClick = save ? handleUnSave : handleSave;
+  function handleUnSave() {
+    setSave(false);
+  }
+  function handleSave() {
+    setSave(true);
+  }
+  return <Icon className={classes.saveIcon} onClick={onClick} />;
+}
+function Comment() {
+  const classes = usePostStyles();
+  const [text, setText] = useState("");
+  return (
+    <div className={classes.commentContainer}>
+      <TextField
+        fullWidth
+        value={text}
+        placeholder="Add a comment"
+        multiline
+        rowsMax={2}
+        rows={1}
+        className={classes.textField}
+        onChange={(event) => setText(event.target.value)}
+        inputProps={{
+          classes: {
+            root: classes.root,
+            underline: classes.underline,
+          },
+        }}
+      />
+      <Button
+        color="primary"
+        className={classes.commentButton}
+        disabled={!text.trim()}
+      >
+        Post
+      </Button>
+    </div>
+  );
+}
 export default Post;
