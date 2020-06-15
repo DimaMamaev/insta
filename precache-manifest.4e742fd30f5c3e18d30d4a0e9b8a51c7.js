6880e25c812b503efeaf606a4a164276b959ce38
DimaMamaev <dmamaev@my.com>
2020-06-15 22:52:52 +0300

    Updates

---
+++ precache-manifest.4e742fd30f5c3e18d30d4a0e9b8a51c7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af69d19e0f65dc9e2234df92ab7b2a2",
+    "url": "/insta/index.html"
+  },
+  {
+    "revision": "793af476c15b5710858e",
+    "url": "/insta/static/css/2.950f1153.chunk.css"
+  },
+  {
+    "revision": "793af476c15b5710858e",
+    "url": "/insta/static/js/2.af8344a3.chunk.js"
+  },
+  {
+    "revision": "e8ccc7cf4c855a56e3a7079a2b405db4",
+    "url": "/insta/static/js/2.af8344a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4553c58c88d233833b",
+    "url": "/insta/static/js/3.a168cfd1.chunk.js"
+  },
+  {
+    "revision": "aaa3ce1a6fa5a390a5fc",
+    "url": "/insta/static/js/main.d9e30af3.chunk.js"
+  },
+  {
+    "revision": "05bb5169c6dd13cac3fa",
+    "url": "/insta/static/js/runtime-main.06b7e88d.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/insta/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/insta/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/insta/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/insta/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/insta/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/insta/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/insta/static/media/slick.f97e3bbf.svg"
+  }
+]);
