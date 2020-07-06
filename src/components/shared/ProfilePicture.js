52c3f9e43b67c7b49249ee0b1707cad808b1afd9
DimaMamaev <dmamaev@my.com>
2020-07-06 21:50:49 +0300

    implemented profile image change functions to profile page

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState, useRef, useContext } from "react";
 import { useProfilePictureStyles } from "../../styles";
-import { defaultCurrentUser } from "../../data";
 import { Person } from "@material-ui/icons";
+import { useMutation } from "@apollo/react-hooks";
+import { UPDATE_USER_PROFILE_AVATAR } from "../../graphql/mutations";
+import handleImageUpload from "../../utils/handleImageUpload";
+import { UserContext } from "../../App";
 
-function ProfilePicture({
-  size,
-  image = defaultCurrentUser.profile_image,
-  isOwner,
-}) {
+function ProfilePicture({ size, image, isOwner }) {
   const classes = useProfilePictureStyles({ size, isOwner });
+  const { currentUserId } = useContext(UserContext);
+  const [profileImage, setProfileImage] = useState(image);
+  const inputRef = useRef();
+  const [editUserAvatar] = useMutation(UPDATE_USER_PROFILE_AVATAR);
+
+  function openFileInput() {
+    inputRef.current.click();
+  }
+  async function handlePicUpdate(event) {
+    const url = await handleImageUpload(event.target.files[0]);
+    const variables = { id: currentUserId, profileImage: url };
+    await editUserAvatar({ variables });
+    setProfileImage(url);
+  }
 
   return (
     <section className={classes.section}>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        ref={inputRef}
+        onChange={handlePicUpdate}
+      />
       {image ? (
-        <div className={classes.wrapper}>
-          <img src={image} alt="User avatar" className={classes.image} />
+        <div
+          className={classes.wrapper}
+          onClick={isOwner ? openFileInput : () => null}
+        >
+          <img src={profileImage} alt="User avatar" className={classes.image} />
         </div>
       ) : (
         <div className={classes.wrapper}>
