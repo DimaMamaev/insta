21f3825f58a6a0273b1cb639b2258cf8b0e3728f
DimaMamaev <dmamaev@my.com>
2020-07-09 22:23:11 +0300

    try to deploy on netlify

---
+++ src/keys.js
@@ -0,0 +1,18 @@
+//  https://insta-clone-app-mamaev.herokuapp.com/console
+//  https://dashboard.heroku.com/apps/insta-clone-app-mamaev
+// https://cloudinary.com/console/c-3c7cac2555e56da48941b16954d8c1
+//  HASURA_GRAPHQL_ADMIN_SECRET   loki
+export const DBuri = "wss://insta-clone-app-mamaev.herokuapp.com/v1/graphql";
+export const DBuriSecret = "loki";
+export const firebaseInitializeData = {
+  apiKey: "AIzaSyCZ6NmpYmMrZhCQvAi4jfVrPxL31Nsz8kA",
+  authDomain: "insta-clone-app-69251.firebaseapp.com",
+  databaseURL: "https://insta-clone-app-69251.firebaseio.com",
+  projectId: "insta-clone-app-69251",
+  storageBucket: "insta-clone-app-69251.appspot.com",
+  messagingSenderId: "817962308734",
+  appId: "1:817962308734:web:a108c45fabc62e6190742f",
+  measurementId: "G-T8MMTRVEW2",
+};
+export const CloudinaryUrl =
+  "https://api.cloudinary.com/v1_1/dmamaev/image/upload";
